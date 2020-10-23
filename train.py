02dce6c5aeb060aa620aea3c131b6644758a30ee
Chen Wang <jeffsan@gmx.com>
2020-10-22 20:28:07 -0400

    update train

--- train.py
+++ train.py
@@ -9,15 +9,17 @@ import warnings
 import argparse
 import numpy as np
 import torch.nn as nn
+import torch.optim as optim
 import torch.utils.data as Data
 
-from datasets import TartanAir
 from models import FeatureNet 
-from models.utils import count_parameters, Timer
+from datasets import TartanAir
+from models.utils import Timer
+from models.utils import count_parameters
+from models.utils import EarlyStopScheduler
 
 
-
-def test(loader, net, device):
+def test(net, loader, device):
     net.eval()
     with torch.no_grad():
         for batch_idx, (image) in enumerate(tqdm.tqdm(loader)):
@@ -27,7 +29,7 @@ def test(loader, net, device):
     return 0.9 # accuracy
 
 
-def train(loader, net, device, creterion):
+def train(net, loader, device, creterion, optimizer):
     net.train()
     for batch_idx, (image, depth, pose) in enumerate(tqdm.tqdm(loader)):
         image, depth, pose = image.to(args.device), depth.to(args.device), pose.to(args.device)
@@ -39,16 +41,19 @@ def train(loader, net, device, creterion):
 if __name__ == "__main__":
     # Arguements
     parser = argparse.ArgumentParser(description='Feature Graph Networks')
-    parser.add_argument("--device", type=str, default='cuda', help="cuda or cpu")
-    parser.add_argument("--data-root", type=str, default='/data/datasets', help="dataset location")
-    parser.add_argument("--dataset", type=str, default='cora', help="cora, citeseer, or pubmed")
-    parser.add_argument("--load", type=str, default=None, help="load pretrained model file")
+    parser.add_argument("--device", type=str, default='cuda:0', help="cuda or cpu")
+    parser.add_argument("--dataset", type=str, default='tartanair', help="TartanAir")
+    parser.add_argument("--data-root", type=str, default='/data/datasets', help="data location")
+    parser.add_argument("--load", type=str, default=None, help="load pretrained model")
     parser.add_argument("--save", type=str, default=None, help="model file to save")
-    parser.add_argument("--optm", type=str, default='SGD', help="SGD or Adam")
     parser.add_argument("--lr", type=float, default=0.01, help="learning rate")
-    parser.add_argument("--epoch", type=int, default=10, help="number of epoches")
+    parser.add_argument("--min-lr", type=float, default=0.001, help="learning rate")
+    parser.add_argument("--factor", type=float, default=0.1, help="factor of lr")
+    parser.add_argument("--momentum", type=float, default=0.9, help="momentum of optim")
+    parser.add_argument("--w-decay", type=float, default=0, help="weight decay of optim")
+    parser.add_argument("--epoch", type=int, default=15, help="number of epoches")
     parser.add_argument("--batch-size", type=int, default=10, help="minibatch size")
-    parser.add_argument("--iteration", type=int, default=5, help="number of training iteration")
+    parser.add_argument("--patience", type=int, default=5, help="training patience")
     parser.add_argument("--seed", type=int, default=0, help='Random seed.')
     args = parser.parse_args(); print(args)
     torch.manual_seed(args.seed)
@@ -59,16 +64,21 @@ if __name__ == "__main__":
     test_data = TartanAir(root=args.data_root, train=False)
     test_loader = Data.DataLoader(test_data, args.batch_size, False)
 
+    criterion = nn.CrossEntropyLoss()
     net = FeatureNet().to(args.device) if args.load is None else torch.load(args.load, args.device)
-
-    creterion = nn.CrossEntropyLoss()
+    optimizer = optim.RMSprop(net.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.w_decay)
+    scheduler = EarlyStopScheduler(optimizer, factor=args.factor, verbose=True, min_lr=args.min_lr, patience=args.patience)
 
     timer = Timer()
     for epoch in range(args.epoch):
-        train_acc = train(train_loader, net, args.device, creterion)
+        train_acc = train(net, train_loader, args.device, criterion, optimizer)
 
         if args.save is not None:
             torch.save(net, args.save)
 
-    test_acc = test(test_loader, net, args.device)
-    print("Train Acc: %.3f, Test Acc: %.3f, Timing: %.2fs"%(train_acc, test_acc, timer.end()))
+        if scheduler.step(1-train_acc):
+            print('Early Stopping!')
+            break
+
+    test_acc = test(net, test_loader, args.device)
+    print("Train: %.3f, Test: %.3f, Timing: %.2fs"%(train_acc, test_acc, timer.end()))
