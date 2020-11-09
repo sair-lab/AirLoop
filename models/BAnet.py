4b4798fb80ae52975605144d48c84f6ed76a67a2
Chen Wang <jeffsan@gmx.com>
2020-11-08 20:27:20 -0500

    minor correction

--- models/BAnet.py
+++ models/BAnet.py
@@ -16,26 +16,26 @@ class BAGDnet(nn.Module):
         self.cx = 320
         self.cy = 240
 
-        self.tMP = torch.nn.Parameter(torch.tensor(MPs[:,1:], dtype=torch.double)).requires_grad_()
-        self.tKF = torch.nn.Parameter(torch.reshape(torch.tensor(KFs[:,1:], dtype=torch.double),(KFs.shape[0],4,4))).requires_grad_()
+        self.tMP = torch.nn.Parameter(MPs[:,1:])
+        self.tKF = torch.nn.Parameter(KFs[:,1:].view(-1,4,4))
 
         # indexing the matches of key frames and Map Point
-        self.idxMP = torch.tensor(MPs[:,0], dtype=torch.int)
-        self.idxKF = torch.tensor(KFs[:,0], dtype=torch.int)
+        self.idxMP = MPs[:,0].type(torch.int)
+        self.idxKF = KFs[:,0].type(torch.int)
 
         # change the point to homogeneous
-        self.tMPhomo = torch.unsqueeze(F.pad(input=self.tMP, pad=(0, 1), mode='constant', value=1), 2)
+        self.tMPhomo = F.pad(input=self.tMP, pad=(0, 1), mode='constant', value=1).unsqueeze(2)
 
         #new pytorch provides inverse function for batch
-        self.invtKF = torch.inverse(self.tKF)
+        self.invtKF = self.tKF.inverse()
 
     def forward(self, measurements):
         #indexing ## it is the same wiht torch where TODO
-        indexKF = torch.where(torch.tensor(measurements[:,0].reshape(-1,1),dtype=torch.int)==self.idxKF)[1]
-        indexMP = torch.where(torch.tensor(measurements[:,1].reshape(-1,1),dtype=torch.int)==self.idxMP)[1]
+        indexKF = torch.where(measurements[:,0].reshape(-1,1).type(torch.int)==self.idxKF)[1]
+        indexMP = torch.where(measurements[:,1].reshape(-1,1).type(torch.int)==self.idxMP)[1]
 
         # In test set, tKF is the Twc inverse. We may need to store something else in Memory
-        self.reprojectPoints = torch.matmul(self.tKF[indexKF],self.tMPhomo[indexMP])
+        self.reprojectPoints = self.tKF[indexKF] @ self.tMPhomo[indexMP]
 
         # This operation can also be a matmul with matrxi K TODO
         self.ptx = (self.reprojectPoints[:,0]/self.reprojectPoints[:,2])*self.fx + self.cx
@@ -43,6 +43,7 @@ class BAGDnet(nn.Module):
 
         obs2d = torch.cat((self.ptx,self.pty),1)
         return obs2d
+
 
 if __name__ == "__main__":
     '''Test codes'''
@@ -53,23 +54,23 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Test BAGD')
     parser.add_argument("--device", type=str, default='cuda', help="cuda, cuda:0, or cpu")
     parser.add_argument('--seed', type=int, default=0, help='Random seed.')
-    parser.add_argument('--lr', type=float, default=0.000001, help='Random seed.')
+    parser.add_argument('--lr', type=float, default=1e-5, help='Random seed.')
     args = parser.parse_args()
     torch.manual_seed(args.seed)
     torch.cuda.manual_seed(args.seed)
 
-    MPs = np.loadtxt("../data/BAtest/MP.txt")
-    KFs = np.loadtxt("../data/BAtest/KF.txt")
-    Matches = np.loadtxt("../data/BAtest/Match.txt")
+    MPs = torch.from_numpy(np.loadtxt("data/BAtest/MP.txt"))
+    KFs = torch.from_numpy(np.loadtxt("data/BAtest/KF.txt"))
+    Matches = torch.from_numpy(np.loadtxt("data/BAtest/Match.txt"))
 
     net = BAGDnet(MPs, KFs)
 
     SmoothLoss = nn.SmoothL1Loss(beta = math.sqrt(5.99))
     optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9)
 
-    z = torch.tensor(Matches[:,2:4])
-    for i in range(20):
-        output = net.forward(Matches)
+    z = Matches[:,2:4]
+    for i in range(200):
+        output = net(Matches)
         loss = SmoothLoss(output,z)
         loss.backward()
         optimizer.step()
